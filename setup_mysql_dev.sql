6a3e7e3fe6e223f95455ebded3ad23d36823a157
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-10-12 02:46:48 -0700

    mysql setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,10 @@
 -- script that prepares a mysql server for the project
 
--- Create a database hbnb_dev_db without the script failing
+-- Create a database hbnb_dev_db without the script failing.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create a user of database
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-
--- set a password to user
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = PASSWORD('hbnb_dev_pwd');
+-- Create a user of database and the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- give all the user's properties to the database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';

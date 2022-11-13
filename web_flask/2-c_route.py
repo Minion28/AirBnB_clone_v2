524bea4778489082cfdde9ddd31481376da9323b
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-11-13 13:55:50 +0300

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''
 do task 1
-/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ” followed by the value of the
+ text variable (replace underscore _ symbols with a space )
 '''
 
 from flask import Flask

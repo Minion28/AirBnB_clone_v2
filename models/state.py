1444810ff65f6945cbc79e126486f547bebf64bb
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-11-14 00:52:57 +0300

    fix

--- models/state.py
+++ models/state.py
@@ -12,18 +12,13 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
+        
         @property
         def cities(self):
             """getter for list of city instances related to the state"""

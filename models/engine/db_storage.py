44afbe27ecefc68c66e9c3257a73df8528d6486c
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-11-14 23:58:56 +0300

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -43,9 +43,9 @@ class DBStorage:
     def all(self, cls=None):
         """query on the current database session"""
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
+        for cls in classes:
+            if cls is None or cls is classes[cls] or cls is cls:
+                objs = self.__session.query(classes[cls]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
+from models.place import Place
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
@@ -15,7 +16,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
@@ -23,8 +24,8 @@ class State(BaseModel, Base):
         def cities(self):
             """getter for list of city instances related to the state"""
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            all_cities = (models.storage.all(City))
+            for city in list(all_cities.values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
